7cda048169c48bfb4b36f3fc77bd3f30b406d8b1
AlJ <alex05.jar76@gmail.com>
2023-10-06 20:43:00 +0300

    new changes

---
+++ test.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+time.sleep(5)
+driver.get("https://stepik.org/lesson/25969/step/12")
+time.sleep(5)
+
+text_area = driver.find_element(By.CSS_SELECTOR, '.textarea')
+
+text_area.send_keys("get()")
+time.sleep(5)
+
+# Найдем кнопку, которая отправляет введенное решение
+submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+
+# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
+submit_button.click()
+time.sleep(5)
+
+# После выполнения всех действий мы должны не забыть закрыть окно браузера
+driver.quit()
