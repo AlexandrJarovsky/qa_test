011fe539fbc66b8c5711abcd4abda95c3d733a87
AlJ <alex05.jar76@gmail.com>
2023-10-06 20:49:50 +0300

    2 commit

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 time.sleep(5)
 driver.get("https://stepik.org/lesson/25969/step/12")
+
 time.sleep(5)
 
 text_area = driver.find_element(By.CSS_SELECTOR, '.textarea')
